6b9d34a3c88dd579e162b7b31acab799079826f6
姚文强 <yaowenqiang111@163.com>
2023-02-20 00:18:26 +0800

    add boost array demo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <boost/array.hpp>
+using namespace std;
+using namespace boost;
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    boost::array<string, 10>  arr;
+    cout << arr.size() << endl;
+
     return 0;
 }
