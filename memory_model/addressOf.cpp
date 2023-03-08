84f0ce06cc10d15abe930d2b50e057420f6c9e31
姚文强 <yaowenqiang111@163.com>
2023-03-08 23:33:49 +0800

    add more signle instance demo

--- memory_model/addressOf.cpp
+++ memory_model/addressOf.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 
+void foo() {
+    int x = 42;
+    std::cout << "x in foo(): " << x << std::endl;
+    std::cout << "&x in foo     : " << &x << std::endl;
+
+    int num = 7;
+    int *p = &num;
+    std::cout << " p: "  << p << std::endl;
+    std::cout << " &p: " << &p << std::endl;
+    std::cout << " *p: "  << *p << std::endl;
+
+    *p = 42;
+
+    std::cout << "*p changed to 42"  <<  std::endl;
+    std::cout << "num: "  <<  num << std::endl;
+
+
+}
+
 int main() {
     int num = 7;
-    std::cout << "Value: " << num << std::endl;
-    std::cout << "Address: " << &num << std::endl;
+    std::cout << "num in main(): " << num << std::endl;
+    std::cout << "&num in main(): " << &num << std::endl;
+    foo();
     return 0;
 }
